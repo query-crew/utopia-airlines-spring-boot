0d00d28b3fa01287a8c6532ce99749faa6024960
chloejohnsonsmoothstack <106343473+chloejohnsonsmoothstack@users.noreply.github.com>
2022-06-29 17:05:07 -0600

    Revert "added users service"

--- src/main/java/com/smoothstack/interfaces/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smoothstack.interfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.smoothstack.models.User;
-public interface UserRepository extends JpaRepository<User, Integer>{
-    
-}
