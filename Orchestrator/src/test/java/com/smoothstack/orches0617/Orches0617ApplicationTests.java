3634a7b13117f2ffccf480d0500bc5b32d817448
Jordan Abbott <abbottjord94@gmail.com>
2022-06-29 17:51:54 -0700

    Added eureka server to back end

---
+++ Orchestrator/src/test/java/com/smoothstack/orches0617/Orches0617ApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.smoothstack.orches0617;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Orches0617ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
